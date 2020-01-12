3dbd641cc2a616dc63eff59d6928f117e964dbfd
vaibhav <vaibhav09121991@gmail.com>
2020-01-12 17:15:47 +0530

    Learning Java (Project initialized)

--- HelloWorld/src/com/codewithvaibhav/Main.java
+++ HelloWorld/src/com/codewithvaibhav/Main.java
@@ -3,7 +3,18 @@ package com.codewithvaibhav;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	// write your code here
         System.out.println("hello world");
+
+        // Primitive Data types
+        byte age = 30;
+        long viewsCount = 23_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        System.out.println(age);
+        System.out.println(viewsCount);
+        System.out.println(price);
+        System.out.println(letter);
     }
 }
